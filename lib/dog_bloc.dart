1f8939e83db4060d534cf8492e3de14460860cc7
Sadu-star <saduisenbek11@gmail.com>
2026-04-14 17:59:08 +0600

    Выполнение домашнего задания 2

--- lib/dog_bloc.dart
+++ lib/dog_bloc.dart
@@ -1,22 +1,14 @@
-import 'package:dio/dio.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:equatable/equatable.dart';
+import 'api_service.dart';
 
 part 'dog_event.dart';
 part 'dog_state.dart';
 
-class ApiService {
-  final Dio _dio = Dio();
-
-  Future<String> getCatImage() async {
-    final Response response = await _dio.get('https://api.thecatapi.com/v1/images/search');
-    return response.data[0]['url'];
-  }
-}
 class DogBloc extends Bloc<DogEvent, DogState> {
-  final ApiService apiService = ApiService();
+  final ApiService apiService;
 
-  DogBloc() : super(DogInitial()) {
+  DogBloc({required this.apiService}) : super(DogInitial()) {
     on<GetDogImageEvent>((event, emit) async {
       try {
         emit(LoadingDogState());
--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,10 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_application_2/dog_bloc.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
 import 'my_home_page.dart'; 
 
 void main() {
   runApp(const MainApp());
 }//В случае ошибки введите в терминал flutter run -d chrome --web-browser-flag "--disable-web-security"
+
 
 class MainApp extends StatelessWidget {
   const MainApp({super.key});
@@ -18,10 +17,7 @@ class MainApp extends StatelessWidget {
         useMaterial3: true,
         colorSchemeSeed: Colors.blue,
       ),
-      home: BlocProvider(
-        create: (context) => DogBloc()..add(GetDogImageEvent()),
-        child: MyHomePage(),
-        )
+      home: const MyHomePage(),
     );
   }
 }
